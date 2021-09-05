3d54e151c8e56bcb0efd92f2e56c16f3178cedbd
jha1Raghu <jha1raghu@gmail.com>
2021-09-05 16:47:50 +0530

    Initial Fresh project

---
+++ app/src/main/java/in/mealpack/MealPack/MainActivity.kt
@@ -0,0 +1,38 @@
+package `in`.mealpack.MealPack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import `in`.mealpack.MealPack.ui.theme.MealPackTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MealPackTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MealPackTheme {
+        Greeting("Android")
+    }
+}
