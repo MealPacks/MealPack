b2ed83d53c029663a3f4bb636ccca1b77c362c9a
jha1Raghu <jha1raghu@gmail.com>
2021-09-13 13:44:58 +0530

    corrected buildSrc Setup : minSdk

--- buildSrc/src/main/kotlin/ConfigVersions.kt
+++ buildSrc/src/main/kotlin/ConfigVersions.kt
@@ -1,9 +1,9 @@
 object ConfigVersions {
     const val appId = "in.mealpack.MealPack"
     const val compileSdk = 31
-    const val minSdk = 31
+    const val minSdk = 21
     const val targetSdk = 31
     const val versionCode = 1
     const val versionName = "0.0.1"
-    const val buildTools = "0.0.1"
+    const val buildTools = "30.0.3"
 }
